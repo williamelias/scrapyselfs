a4f47ae1bfce9f1eb5699c37f470019cc8a71509
williamelias <williameliasalves@gmail.com>
2023-11-02 16:38:02 -0300

    feat: create service and instance to Scrapy object.

---
+++ scrapyself-front-end/src/app/scraping-instance.ts
@@ -0,0 +1,5 @@
+export interface ScrapingInstance {
+    url:string | undefined;
+    withProxy:boolean | undefined;
+    integrationType:string | undefined;
+}
--- scrapyself-front-end/src/app/scraping-service.service.ts
+++ scrapyself-front-end/src/app/scraping-service.service.ts
@@ -1,24 +1,43 @@
 import { Injectable } from '@angular/core';
+import { ScrapingInstance } from './scraping-instance';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScrapingService {
 
+  url: string = 'http://127.0.0.1:8000/'
+  headers: HeadersInit = {
+    "Content-type": "application/json;charset=UTF-8"
+  }
   constructor() { }
-  url:string = 'http://127.0.0.1:8000/'
 
-  async getData(){
-    try{
+  async getData() {
+    try {
       const response = await fetch(this.url)
       console.log(response.status)
       const respJson = await response.json()
       console.log(`response:${respJson}`)
       return respJson
     }
-    catch(error){
+    catch (error) {
       console.error(error)
-      return {'error': `${error}`}
+      return { 'error': `${error}` }
+    }
+  }
+  async validate(scrapingInstance: ScrapingInstance) {
+
+    try {
+
+      const response = await fetch(this.url + 'validate', {
+        method: "POST",
+        body: JSON.stringify(scrapingInstance),
+        headers: this.headers
+      })
+      return await response.json()
+    }
+    catch (error) {
+      console.log(error)
     }
   }
 }
