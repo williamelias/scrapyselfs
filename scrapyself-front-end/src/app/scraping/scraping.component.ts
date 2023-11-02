83c70a4d801afe2fb00334dabd98c40c9cb8a427
williamelias <williameliasalves@gmail.com>
2023-11-02 16:38:49 -0300

    refact: rename things and create validation method that call service.

--- scrapyself-front-end/src/app/scraping/scraping.component.ts
+++ scrapyself-front-end/src/app/scraping/scraping.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ScrapingService } from '../scraping-service.service';
+import { ScrapingInstance } from '../scraping-instance';
 
 @Component({
   selector: 'app-scraping',
@@ -9,9 +10,9 @@ import { ScrapingService } from '../scraping-service.service';
 })
 export class ScrapingComponent {
   scrapingForm = new FormGroup({
-    url : new FormControl('',Validators.required),
-    integrationType: new FormControl('',Validators.required),
-    proxySetting: new FormControl('', Validators.required)
+    url : new FormControl('',[Validators.required,Validators.nullValidator]),
+    integrationType: new FormControl('',[Validators.required,Validators.nullValidator]),
+    withProxy: new FormControl('', [Validators.required,Validators.nullValidator])
   })
 
   scrapingService = new ScrapingService();
@@ -20,6 +21,24 @@ export class ScrapingComponent {
     console.log(`value: ${this.scrapingForm.value}`)
     this.scrapingService.getData().then(
       (message)=> console.log(`data::: ${JSON.stringify(message)}`)
+    )
+  }
+
+  validateData(){
+    console.log(`value: ${this.scrapingForm.value}`)
+
+
+    let scrapingInstance: ScrapingInstance = {
+      url:this.scrapingForm.value.url!,
+      integrationType:this.scrapingForm.value.integrationType!,
+      withProxy: this.scrapingForm.value.withProxy! == 'yes'?true:false
+    }
+
+
+    this.scrapingService.validate(
+      scrapingInstance
+    ).then(
+      (data)=> console.log(`data:${JSON.stringify(data)}`)
     )
   }
 }
